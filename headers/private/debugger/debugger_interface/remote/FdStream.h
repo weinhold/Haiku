ebcc5123c4f4739a744c4dc280ddff585671d489
Ingo Weinhold <ingo_weinhold@gmx.de>
2017-08-01 09:40:21 +0200

    Debugger: + FdStream
    A Stream implementation using two FDs (for input and output).

---
+++ headers/private/debugger/debugger_interface/remote/FdStream.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+#pragma once
+
+
+#include "Stream.h"
+
+
+struct FdStream : Stream {
+								FdStream(int input, int output,
+									bool closeInput = false,
+									bool closeOutput = false);
+	virtual						~FdStream();
+
+	virtual	ssize_t				Read(void* buffer, size_t size);
+	virtual	ssize_t				Write(const void* buffer, size_t size);
+
+	virtual	status_t			Flush();
+
+	virtual	void				Close();
+
+private:
+			int					fInput;
+			int					fOutput;
+			bool				fCloseInput;
+			bool				fCloseOutput;
+};
---
+++ src/kits/debugger/debugger_interface/remote/FdStream.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017, Ingo Weinhold, ingo_weinhold@gmx.de.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include "FdStream.h"
+
+#include <errno.h>
+#include <fcntl.h>
+
+
+FdStream::FdStream(int input, int output, bool closeInput, bool closeOutput)
+	:
+	fInput(input),
+	fOutput(output),
+	fCloseInput(closeInput),
+	fCloseOutput(closeOutput)
+{
+}
+
+
+FdStream::~FdStream()
+{
+	Close();
+}
+
+
+ssize_t
+FdStream::Read(void* buffer, size_t size)
+{
+	ssize_t bytesRead = read(fInput, buffer, size);
+	if (bytesRead < 0)
+		return errno;
+	return bytesRead;
+}
+
+
+ssize_t
+FdStream::Write(const void* buffer, size_t size)
+{
+	ssize_t bytesWritten = write(fOutput, buffer, size);
+	if (bytesWritten < 0)
+		return errno;
+	return bytesWritten;
+}
+
+
+status_t
+FdStream::Flush()
+{
+	return B_OK;
+}
+
+
+void
+FdStream::Close()
+{
+	if (fInput >= 0 && fCloseInput) {
+		close(fInput);
+		fInput = -1;
+	}
+
+	if (fOutput >= 0 && fCloseOutput) {
+		close(fOutput);
+		fOutput = -1;
+	}
+}
