10b64fe61bc4641c24fea4006a21643740ed317c
Ingo Weinhold <ingo_weinhold@gmx.de>
2018-01-08 20:06:05 +0000

    Debugger: Tracing.h: log to stderr instead of stdout

--- headers/private/debugger/Tracing.h
+++ headers/private/debugger/Tracing.h
@@ -16,7 +16,7 @@
 
 
 #if APPS_DEBUGGER_TRACE_DWARF_DIE
-#	define TRACE_DIE(x...)		printf(x)
+#	define TRACE_DIE(x...)		fprintf(stderr, x)
 #	define TRACE_DIE_ONLY(x)	x
 #else
 #	define TRACE_DIE(x...)		(void)0
@@ -24,7 +24,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_DWARF_LINE_INFO
-#	define TRACE_LINES(x...)	printf(x)
+#	define TRACE_LINES(x...)	fprintf(stderr, x)
 #	define TRACE_LINES_ONLY(x)	x
 #else
 #	define TRACE_LINES(x...)	(void)0
@@ -32,7 +32,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_DWARF_LINE_INFO >= 2
-#	define TRACE_LINES2(x...)	printf(x)
+#	define TRACE_LINES2(x...)	fprintf(stderr, x)
 #	define TRACE_LINES2_ONLY(x)	x
 #else
 #	define TRACE_LINES2(x...)	(void)0
@@ -40,7 +40,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_DWARF_EXPRESSIONS
-#	define TRACE_EXPR(x...)	printf(x)
+#	define TRACE_EXPR(x...)	fprintf(stderr, x)
 #	define TRACE_EXPR_ONLY(x)	x
 #else
 #	define TRACE_EXPR(x...)	(void)0
@@ -48,7 +48,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_DWARF_PUBLIC_TYPES
-#	define TRACE_PUBTYPES(x...)	printf(x)
+#	define TRACE_PUBTYPES(x...)	fprintf(stderr, x)
 #	define TRACE_PUBTYPES_ONLY(x)	x
 #else
 #	define TRACE_PUBTYPES(x...)	(void)0
@@ -56,7 +56,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_CFI
-#	define TRACE_CFI(x...)		printf(x)
+#	define TRACE_CFI(x...)		fprintf(stderr, x)
 #	define TRACE_CFI_ONLY(x)	x
 #else
 #	define TRACE_CFI(x...)		(void)0
@@ -64,7 +64,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_STACK_FRAME_LOCALS
-#	define TRACE_LOCALS(x...)	printf(x)
+#	define TRACE_LOCALS(x...)	fprintf(stderr, x)
 #	define TRACE_LOCALS_ONLY(x)	x
 #else
 #	define TRACE_LOCALS(x...)	(void)0
@@ -72,7 +72,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_IMAGES
-#	define TRACE_IMAGES(x...)	printf(x)
+#	define TRACE_IMAGES(x...)	fprintf(stderr, x)
 #	define TRACE_IMAGES_ONLY(x)	x
 #else
 #	define TRACE_IMAGES(x...)	(void)0
@@ -80,7 +80,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_CODE
-#	define TRACE_CODE(x...)		printf(x)
+#	define TRACE_CODE(x...)		fprintf(stderr, x)
 #	define TRACE_CODE_ONLY(x)	x
 #else
 #	define TRACE_CODE(x...)		(void)0
@@ -88,7 +88,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_JOBS
-#	define TRACE_JOBS(x...)		printf(x)
+#	define TRACE_JOBS(x...)		fprintf(stderr, x)
 #	define TRACE_JOBS_ONLY(x)	x
 #else
 #	define TRACE_JOBS(x...)		(void)0
@@ -96,7 +96,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_DEBUG_EVENTS
-#	define TRACE_EVENTS(x...)	printf(x)
+#	define TRACE_EVENTS(x...)	fprintf(stderr, x)
 #	define TRACE_EVENTS_ONLY(x)	x
 #else
 #	define TRACE_EVENTS(x...)	(void)0
@@ -104,7 +104,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_TEAM_CONTROL
-#	define TRACE_CONTROL(x...)	printf(x)
+#	define TRACE_CONTROL(x...)	fprintf(stderr, x)
 #	define TRACE_CONTROL_ONLY(x)	x
 #else
 #	define TRACE_CONTROL(x...)	(void)0
@@ -112,7 +112,7 @@
 #endif
 
 #if APPS_DEBUGGER_TRACE_GUI
-#	define TRACE_GUI(x...)		printf(x)
+#	define TRACE_GUI(x...)		fprintf(stderr, x)
 #	define TRACE_GUI_ONLY(x)	x
 #else
 #	define TRACE_GUI(x...)		(void)0
